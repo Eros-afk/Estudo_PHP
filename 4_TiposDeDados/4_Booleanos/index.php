dfa0a0b8892d8b28b6b10bca1245a10131be7743
Eros-afk <erosryansimette@alu.ufc.br>
2026-03-18 08:25:00 -0300

    #

---
+++ 4_TiposDeDados/4_Booleanos/index.php
@@ -0,0 +1,17 @@
+<?php
+    echo true;
+    echo "<br>";
+    echo false;
+
+    if (true){
+        echo "É verdadeiro <br>";
+    }
+
+    if (false){
+        echo "É falso <br>";
+    }
+
+    $a = false;
+    if ($a){
+        echo "É falso";
+    }
